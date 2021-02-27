ddae5d99d113731750e012978ed76f83f10af903
zhangzhenQuan <1726891762@qq.com>
2021-02-27 14:09:46 +0800

    初始化项目

---
+++ src/main/java/com/whoami/algorithmdemo/AlgorithmDemoApplication.java
@@ -0,0 +1,13 @@
+package com.whoami.algorithmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmDemoApplication.class, args);
+	}
+
+}
